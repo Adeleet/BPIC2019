c297fbd7f62448d9b0c33553686af16c4fd18f98
Max Adaloudis <maxadaloudis@gmail.com>
2019-05-18 15:09:57 +0200

    Removes tests script for quick conversion of xes to csv

--- Scripts/XES_to_CSV.py
+++ Scripts/XES_to_CSV.py
@@ -9,15 +9,5 @@ def xes_to_csv(xes_path, csv_path):
         xes_path (str) : path of .xes compliant input event log
         csv_path (str) : path of .csv output event log
     """
-    log = xes_import_factory.apply(f_path)
-    csv_exporter.export(log, f"alignment-{cat}.csv")
-
-
-# Using the 4 created alignments for each matching categories, specified in the ALIGNMENTS_PATH
-MATCHING_CATEGORIES = ["2-way", "3-way-before-GR",
-                       "3-way-after-GR", "consignment"]
-ALIGNMENTS_PATH = "./Data/Alignments"
-
-for cat in MATCHING_CATEGORIES:
-    xes_to_csv(xes_path=f"{ALIGNMENTS_PATH}/alignment-{cat}.xes",
-               csv_path=f"{ALIGNMENTS_PATH}/alignment-{cat}.csv")
+    log = xes_import_factory.apply(xes_path)
+    csv_exporter.export(log, csv_path)
